cdd2a8d2f1b37845ea7111b9fc03aee7a192567a
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-07-16 00:06:58 +0530

    Added responsive toggle to navbar bw hamburger & expanded view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,19 +8,23 @@ import './css/navbar.css';
 
 const PageNavbar = () =>{
     return (
-        <Navbar variant="dark" fixed="top">
-            <Navbar.Brand href="#home">Boundary Goods</Navbar.Brand>
-
-            <Nav className="ml-auto" style={{marginRight:"2%"}}>
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Categories</Nav.Link>
-                <Nav.Link href="#pricing">Contribute</Nav.Link>
-            </Nav>             
+        <Navbar collapseOnSelect variant="dark" fixed="top" expand="lg">
             
-            <Form inline>
-                <FormControl type="text" placeholder="Search item" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
-            </Form>  
+            <Navbar.Brand href="#home">Boundary Goods</Navbar.Brand>
+            <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav" />
+          
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto" style={{marginRight:"2%"}}>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#categories">Categories</Nav.Link>
+                    <Nav.Link href="#contribute">Contribute</Nav.Link>
+                </Nav> 
+                <Form inline>
+                    <FormControl type="text" placeholder="Search item" className="mr-sm-2" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>                  
+            </Navbar.Collapse>
+            
         </Navbar>
     );
 };
