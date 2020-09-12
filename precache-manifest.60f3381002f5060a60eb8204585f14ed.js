d9eba936f952d0ebfd1b36d409d8f7c6d192f450
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-09-12 15:13:40 +0530

    Updates

---
+++ precache-manifest.60f3381002f5060a60eb8204585f14ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9832eef805026034e856e78cfb64622",
+    "url": "/goods-listing-template/index.html"
+  },
+  {
+    "revision": "910f3f6dca8748aced2c",
+    "url": "/goods-listing-template/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3b90f5453bd164b1db9a",
+    "url": "/goods-listing-template/static/css/main.800ef415.chunk.css"
+  },
+  {
+    "revision": "910f3f6dca8748aced2c",
+    "url": "/goods-listing-template/static/js/2.889d5331.chunk.js"
+  },
+  {
+    "revision": "e4c137de68f1dc6b72d1a2fbfaf8fb8f",
+    "url": "/goods-listing-template/static/js/2.889d5331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b90f5453bd164b1db9a",
+    "url": "/goods-listing-template/static/js/main.9afaa5c3.chunk.js"
+  },
+  {
+    "revision": "95ce540fcccf65d6d3c8",
+    "url": "/goods-listing-template/static/js/runtime-main.193300e8.js"
+  }
+]);
